feb5d973ac3cf74b1f959338167fe32953474554
Esteban Boucher <es.boucher@protomail.com>
2021-01-07 12:25:22 +0100

    Added selectors, but still not using them in the parse method

--- WoolSupComp/spiders/wollplatz_spyder.py
+++ WoolSupComp/spiders/wollplatz_spyder.py
@@ -31,7 +31,28 @@ class WollplatzSpyder(scrapy.Spider):
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log(f'Saved file {filename}')
-        # for wool_ball in response.xpath("//div[@class='wool']"):
-        #     yield {
-        #         'Author': wool_ball.xpath('.//span/a/@href').get(),
-        #     }
+        for wool_ball in response.xpath('//span/text()'):
+            yield {
+                'Product price currency': wool_ball.xpath('//div[contains(@class, "shopholder")]//*[@id="ContentPlaceHolder1_pnlPDetailBuyHolder"]//div[contains(@class, "buy-price")]//span[@class="product-price"]//span[@class="product-price-currency"]').get(),
+                'Product price amount': wool_ball.xpath('//div[contains(@class, "shopholder")]//*[@id="ContentPlaceHolder1_pnlPDetailBuyHolder"]//div[contains(@class, "buy-price")]//span[@class="product-price"]//span[@class="product-price-amount"]').get(),
+                'Needle size': wool_ball.xpath('//div[contains(@class, "shopholder")]//*[@id="ContentPlaceHolder1_pnShortSpecs"]//div[contains(@class, "innerspecsholder")]//*[@id="pdetailTableSpecs"]//table//tbody//tr[td//text()[contains(., "Nadelstärke")]]').get(),
+                'Composition': wool_ball.xpath('//div[contains(@class, "shopholder")]//*[@id="ContentPlaceHolder1_pnShortSpecs"]//div[contains(@class, "innerspecsholder")]//*[@id="pdetailTableSpecs"]//table//tbody//tr[td//text()[contains(., "Zusammenstellung")]]').get(),
+            }
+
+
+
+# Selector for class="product-price-currency":
+# //div[contains(@class, "shopholder")]//*[@id="ContentPlaceHolder1_pnlPDetailBuyHolder"]//div[contains(@class, "buy-price")]//span[@class="product-price"]//span[@class="product-price-currency"]
+
+# Selector for class="product-price-amount":
+# //div[contains(@class, "shopholder")]//*[@id="ContentPlaceHolder1_pnlPDetailBuyHolder"]//div[contains(@class, "buy-price")]//span[@class="product-price"]//span[@class="product-price-amount"]
+
+
+# Selector for needle size
+# //div[contains(@class, "shopholder")]//*[@id="ContentPlaceHolder1_pnShortSpecs"]//div[contains(@class, "innerspecsholder")]//*[@id="pdetailTableSpecs"]//table//tbody//tr[td//text()[contains(., "Nadelstärke")]]
+
+# Selector for Composition
+# //div[contains(@class, "shopholder")]//*[@id="ContentPlaceHolder1_pnShortSpecs"]//div[contains(@class, "innerspecsholder")]//*[@id="pdetailTableSpecs"]//table//tbody//tr[td//text()[contains(., "Zusammenstellung")]]
+
+# Selector for delivery time
+# (information not found in website, except for "Between 3 to 5 work days" by default for all products)
